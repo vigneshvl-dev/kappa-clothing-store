2e0b3a8d13231283944399ffc1039410052fa434
vigneshvl-dev <vigneshvl.cs25@stellamaryscoe.edu.in>
2026-07-19 18:41:27 +0530

    GGF

--- api/create-order.js
+++ api/create-order.js
@@ -1,8 +1,8 @@
 const Razorpay = require("razorpay");
 
 const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
+    key_id: "rzp_test_TFGK0UTin09QQd",
+    key_secret: "2e3X86oDqAnwEHpOTn5BO6uG",
 });
 
 module.exports = async (req, res) => {
