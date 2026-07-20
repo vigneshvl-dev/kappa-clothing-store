9a062c636ccfc733996e41a19b9cf78d88ab1fc2
Jashwin J <jashwinj2008@gmail.com>
2026-07-20 22:54:30 +0530

    change

--- supabase/functions/create-order/index.ts
+++ supabase/functions/create-order/index.ts
@@ -14,12 +14,8 @@ serve(async (req) => {
   try {
     const { amount, receipt_id } = await req.json()
 
-    const keyId = Deno.env.get('RAZORPAY_KEY_ID')
-    const keySecret = Deno.env.get('RAZORPAY_KEY_SECRET')
-
-    if (!keyId || !keySecret) {
-      throw new Error("Razorpay environment keys are missing on the server.")
-    }
+    const keyId = 'rzp_test_TFOxCSs3iPI2pU';
+    const keySecret = 'bb6nZdz3nr7lytEq9eaubKg6';
 
     const razorpayRes = await fetch('https://api.razorpay.com/v1/orders', {
       method: 'POST',
